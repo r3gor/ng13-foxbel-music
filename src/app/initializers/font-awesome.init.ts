9225a1dd867ba4f7e0a3d23f818dbf9e8c231f70
Roger Ramos <roger.ramos@gdpteam.com>
2022-07-13 03:18:29 -0500

    feat: menu component styles

--- src/app/initializers/font-awesome.init.ts
+++ src/app/initializers/font-awesome.init.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faSquare, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+import { faSquare, faCheckSquare, faSquareXmark } from '@fortawesome/free-solid-svg-icons';
 import { faSquare as farSquare, faCheckSquare as farCheckSquare } from '@fortawesome/free-regular-svg-icons';
 import { faStackOverflow, faGithub, faMedium, faDeezer } from '@fortawesome/free-brands-svg-icons';
 import { Initializer } from "./abstract-initializer";
@@ -27,6 +27,7 @@ export class FontAwesomeInit implements Initializer {
       faGithub,
       faMedium,
       faDeezer,
+      faSquareXmark,
     );
   }
 
